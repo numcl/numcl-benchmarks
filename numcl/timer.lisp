4cca604c97fc6869b6f2988f1f3c3e24091932d0
Masataro Asai <guicho2.71828@gmail.com>
2019-09-16 13:14:44 -0400

    more consistent printing

--- numcl/timer.lisp
+++ numcl/timer.lisp
@@ -24,46 +24,48 @@
            (push real-time-ms real-time-ms-s)
            (push gc-run-time-ms gc-run-time-ms-s)))
        fn))
-    (list (float (/ (reduce #'+ real-time-ms-s)       1000))
-          (+ (float (/ (reduce #'+ user-run-time-us-s)   1000000))
-             (float (/ (reduce #'+ system-run-time-us-s) 1000000)))
-          (float (/ (reduce #'+ user-run-time-us-s)   1000000))
-          (float (/ (reduce #'+ system-run-time-us-s) 1000000))
-          (float (/ (reduce #'+ gc-run-time-ms-s)     1000))
+    (flet ((digits (x) (multiple-value-list (floor x 1000))))
+      (list (list (digits (reduce #'+ real-time-ms-s))
+                  (digits (+ (floor (reduce #'+ user-run-time-us-s)   1000)
+                             (floor (reduce #'+ system-run-time-us-s) 1000)))
+                  (digits (floor (reduce #'+ user-run-time-us-s) 1000))
+                  (digits (floor (reduce #'+ system-run-time-us-s) 1000))
+                  (digits (reduce #'+ gc-run-time-ms-s)))
 
-          (float (/ (alexandria:median real-time-ms-s)       1000))
-          (+ (float (/ (alexandria:median user-run-time-us-s)   1000000))
-             (float (/ (alexandria:median system-run-time-us-s) 1000000)))
-          (float (/ (alexandria:median user-run-time-us-s)   1000000))
-          (float (/ (alexandria:median system-run-time-us-s) 1000000))
-          (float (/ (alexandria:median gc-run-time-ms-s)     1000))
+            (list (digits (round (alexandria:median real-time-ms-s)))
+                  (digits (round (+ (floor (alexandria:median user-run-time-us-s)   1000)
+                                    (floor (alexandria:median system-run-time-us-s) 1000))))
+                  (digits (floor (round (alexandria:median user-run-time-us-s)) 1000))
+                  (digits (floor (round (alexandria:median system-run-time-us-s)) 1000))
+                  (digits (round (alexandria:median gc-run-time-ms-s))))
 
-          (float (/ (reduce #'max real-time-ms-s :initial-value 0)       1000))
-          (+ (float (/ (reduce #'max user-run-time-us-s :initial-value 0)   1000000))
-             (float (/ (reduce #'max system-run-time-us-s :initial-value 0) 1000000)))
-          (float (/ (reduce #'max user-run-time-us-s :initial-value 0)   1000000))
-          (float (/ (reduce #'max system-run-time-us-s :initial-value 0) 1000000))
-          (float (/ (reduce #'max gc-run-time-ms-s :initial-value 0)     1000)))))
+            (list (digits (reduce #'max real-time-ms-s :initial-value 0))
+                  (digits (+ (floor (reduce #'max user-run-time-us-s :initial-value 0)   1000)
+                             (floor (reduce #'max system-run-time-us-s :initial-value 0) 1000)))
+                  (digits (floor (reduce #'max user-run-time-us-s :initial-value 0) 1000))
+                  (digits (floor (reduce #'max system-run-time-us-s :initial-value 0) 1000))
+                  (digits (reduce #'max gc-run-time-ms-s :initial-value 0)))))))
 
 (defmacro with-timing ((&key (trials +trials+) (name "N/A")) &body body)
   (when (< *header-width* (length name))
     (setf name (subseq name 0 *header-width*)))
   `(progn
-     (format *trace-output* "~va ~{ ~6,4f  ~}~%"
+     ;; the numbers are milliseconds so we just print 0 for the fourth digit below the dot
+     (format *trace-output* "~va ~{~:{ ~4@a.~3,,,'0@a0  ~} ~}~%"
              ,*header-width* ,name
              (call-with-benchmark ,trials (lambda () ,@body)))
      (finish-output *trace-output*)))
 
 (defun banner ()
-  (format *trace-output* "~va ~@{~22@a                      |~}~%"
+  (format *trace-output* "~va|~@{~30@a                              |~}~%"
           *header-width*
           "##"
           'sum
           'median
           'max)
-  (format *trace-output* "~va ~@{ ~6a  ~}~%"
+  (format *trace-output* "~va|~{~{ ~9@a  ~}|~}~%"
           *header-width*
           "##"
-          'real 'total 'usr 'sys 'gc
-          'real 'total 'usr 'sys 'gc
-          'real 'total 'usr 'sys 'gc))
+          '((real total usr sys gc)
+            (real total usr sys gc)
+            (real total usr sys gc))))
