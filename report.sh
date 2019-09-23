d8ce83e96263e2a6cbff5bc0262768f743993192
Masataro Asai <guicho2.71828@gmail.com>
2019-09-23 13:48:11 -0400

    reporter

---
+++ report.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+make -j 4
+
+titles (){
+    cat numcl/logs/*.log numpy/logs/*.log | awk -f report.awk | cut -f1 | sort | uniq
+}
+     
+collect_numcl (){
+    titles | while read title
+    do
+        cat numcl/logs/*.log | awk -f report.awk | (grep -m 1 $title || echo "N/A" ) | cut -f2
+    done
+}
+
+collect_numpy (){
+    titles | while read title
+    do
+        cat numpy/logs/*.log | awk -f report.awk | (grep -m 1 $title || echo "N/A" ) | cut -f2
+    done
+}
+
+(
+    echo "title numcl numpy"
+    paste <(titles) \
+          <(collect_numcl) \
+          <(collect_numpy)
+) | column -t | tee $(date +%Y%m%d%H%M).log
