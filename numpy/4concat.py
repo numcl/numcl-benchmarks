3ddb8f3486012f977dc662735a99bc0b82460e8d
Masataro Asai <guicho2.71828@gmail.com>
2019-09-22 22:42:09 -0400

    concatenate / stack for numpy

---
+++ numpy/4concat.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+
+# This file is a part of NUMCL project.
+# Copyright (c) 2019 IBM Corporation
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# 
+# NUMCL is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# NUMCL is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# NUMCL.  If not, see <http://www.gnu.org/licenses/>.
+
+from numpy import *
+from benchmarker import Benchmarker, Reporter
+
+class Short(Reporter):
+    def __init__(self):
+        super().__init__(20)
+    def report_ranking(self, benchmarks):
+        return ''
+    def report_matrix(self, benchmarks):
+        return ''
+
+with Benchmarker(loop=100, filter="tag!=slow", reporter=Short()) as bench:
+
+    a = zeros((100,100,100))
+    b = zeros((100,100,100))
+
+    @bench('concatenate/0')
+    def run(bm):
+        for i in bm:
+            concatenate((a, b), axis=0)
+    @bench('concatenate/1')
+    def run(bm):
+        for i in bm:
+            concatenate((a, b), axis=1)
+    @bench('concatenate/2')
+    def run(bm):
+        for i in bm:
+            concatenate((a, b), axis=2)
+    @bench('stack/0')
+    def run(bm):
+        for i in bm:
+            stack((a, b), axis=0)
+    @bench('stack/1')
+    def run(bm):
+        for i in bm:
+            stack((a, b), axis=1)
+    @bench('stack/2')
+    def run(bm):
+        for i in bm:
+            stack((a, b), axis=2)
+    # @bench('split/0')
+    # def run(bm):
+    #     for i in bm:
+    #         split(a, axis=0)
+    # @bench('split/1')
+    # def run(bm):
+    #     for i in bm:
+    #         split(a, axis=1)
+    # @bench('split/2')
+    # def run(bm):
+    #     for i in bm:
+    #         split(a, axis=2)
