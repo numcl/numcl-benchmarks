a9537b351b240aac6d7f3644b208fc1fe7feb54e
Masataro Asai <guicho2.71828@gmail.com>
2019-09-22 22:58:11 -0400

    math

---
+++ numpy/5math.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python3
+
+# This file is a part of NUMCL project.
+# Copyright (c) 2019 IBM Corporation
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# 
+# NUMCL is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# NUMCL is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# NUMCL.  If not, see <http://www.gnu.org/licenses/>.
+
+from numpy import *
+from benchmarker import Benchmarker, Reporter
+
+class Short(Reporter):
+    def __init__(self):
+        super().__init__(20)
+    def report_ranking(self, benchmarks):
+        return ''
+    def report_matrix(self, benchmarks):
+        return ''
+
+with Benchmarker(loop=100, filter="tag!=slow", reporter=Short()) as bench:
+
+    a = zeros((100,100,100))
+    b = ones ((100,100,100))
+
+    @bench('sin/0')
+    def run(bm):
+        for i in bm:
+            sin(a)
+    @bench('cos/0')
+    def run(bm):
+        for i in bm:
+            cos(a)
+    @bench('tan/0')
+    def run(bm):
+        for i in bm:
+            tan(a)
+    @bench('sinh/0')
+    def run(bm):
+        for i in bm:
+            sinh(a)
+    @bench('cosh/0')
+    def run(bm):
+        for i in bm:
+            cosh(a)
+    @bench('tanh/0')
+    def run(bm):
+        for i in bm:
+            tanh(a)
+    @bench('asin/0')
+    def run(bm):
+        for i in bm:
+            arcsin(a)
+    @bench('acos/0')
+    def run(bm):
+        for i in bm:
+            arccos(a)
+    @bench('atan/0')
+    def run(bm):
+        for i in bm:
+            arctan(a)
+    @bench('exp/0')
+    def run(bm):
+        for i in bm:
+            exp(a)
+    @bench('log/0')
+    def run(bm):
+        for i in bm:
+            log(b)
